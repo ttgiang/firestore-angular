55ca05927194b9e52279d1063e3c28b76c763778
ttgiang <ttgiang@gmail.com>
2021-02-25 07:51:58 -1000

    Cleaning

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,14 @@
 
 export const environment = {
   production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBoViy_OcYhibcfk-yjQ3cgbUaPEZ2GECc",
+    authDomain: "coffees-7e382.firebaseapp.com",
+    projectId: "coffees-7e382",
+    storageBucket: "coffees-7e382.appspot.com",
+    messagingSenderId: "958411586586",
+    appId: "1:958411586586:web:a93bc868bbfab3f4cea5a3",
+  },
 };
 
 /*
